58de4c78ce804a34d357f02bb5e0c1ec67cedb01
Jack Zhang <954294627@qq.com>
2022-06-11 19:03:47 +0800

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # GameShell-Scripts
 Upgrade the functionalities of your GameShell.
+
+## Quick Usage
+
+Upgrade graphics:
+
+```bash
+bash <(curl -s https://raw.githubusercontent.com/zzxzzk115/GameShell-Scripts/main/upgrade_graphics.sh)
+```
+
+## License
+
+The project is under the [MIT](./LICENSE) License.
--- upgrade_graphics.sh
+++ upgrade_graphics.sh
@@ -4,7 +4,8 @@
 sudo apt-get install ninja-build
 
 # Build and install CMake 3.23.1
-wget https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1.tar.gz
+cd ~
+wget -P . -m -nH https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1.tar.gz
 tar -xzvf cmake-3.23.1.tar.gz
 cd cmake-3.23.1
 ./bootstrap --prefix=/usr/ && make -j4 && sudo make install
@@ -15,7 +16,7 @@ sudo pip3 install meson
 
 # Build and install libdrm 2.4.110
 cd ~
-wget https://dri.freedesktop.org/libdrm/libdrm-2.4.110.tar.xz
+wget -P . -m -nH https://dri.freedesktop.org/libdrm/libdrm-2.4.110.tar.xz
 xz -d  libdrm-2.4.110.tar.xz
 tar -xf libdrm-2.4.110.tar
 cd libdrm-2.4.110/
@@ -27,7 +28,7 @@ cd ~
 sudo apt-get install g++ python3-mako zlib1g-dev libexpat1-dev flex bison libx11-dev libxext-dev libxdamage-dev libxcb-glx0-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev libxshmfence-dev libxxf86vm-dev libxrandr-dev libunwind-dev llvm
 
 # Build and install Mesa 22.1
-wget https://archive.mesa3d.org/mesa-22.1.0.tar.xz
+wget -P . -m -nH https://archive.mesa3d.org/mesa-22.1.0.tar.xz
 xz -d mesa-22.1.0.tar.xz
 tar -xf mesa-22.1.0.tar
 cd mesa-22.1.0
