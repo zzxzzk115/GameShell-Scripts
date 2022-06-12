ebdd01ede686121f5feae05240b97207eeda9569
zhangkexuan <zhangkexuan@blackjack-inc.com>
2022-06-12 13:55:33 +0800

    [0.1.1] Fixed bugs.

--- upgrade_graphics.sh
+++ upgrade_graphics.sh
@@ -4,7 +4,7 @@
 cd ~
 mkdir -p tmp
 cd tmp
-wget https://github.com/zzxzzk115/GameShell-Scripts/releases/download/v0.1.0/prebuild.zip
+wget https://github.com/zzxzzk115/GameShell-Scripts/releases/download/v0.1.1/prebuild.zip
 unzip prebuild.zip
 
 # Install libdrm
